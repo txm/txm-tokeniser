a5640c5e2cc6e5fa89e3f595cc98e3bd554d1ad9
Andrew McGregor <andy@txm.net>
2015-11-18 12:11:28 +0000

    bug(txm): mv tokeniser.js index.js; include npm pkg

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.warn('index.js')
+(function () {
+  "use strict"
+
+  var replaceStream = require('replacestream')
+  var fs = require('fs')
+  var path = require('path')
+
+
+  module.exports = {
+
+    tokenise: function(args, callback) {
+
+      var err
+
+      fs.createReadStream(args.src_file)
+        .pipe(replaceStream(args.search, args.replace))
+        .pipe(fs.createWriteStream(args.dst_file))
+
+      callback(err)
+
+    }
+
+  }
+
+}())
